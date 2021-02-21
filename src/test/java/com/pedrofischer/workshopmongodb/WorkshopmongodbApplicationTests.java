81389aca8daf89aae1392b5fa9142d43fc9da90a
pedro fischer <pedro.fischer22@gmail.com>
2021-02-21 14:45:27 -0300

    [add] adicionando novo projeto

---
+++ src/test/java/com/pedrofischer/workshopmongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrofischer.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
